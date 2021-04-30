fd588851e93457ada75471cfcfe96ab193c3808a
mthang1801 <mthang1801@gmail.com>
2021-04-30 20:26:49 +0700

    adding gatsby plugin sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,34 @@ module.exports = {
     `gatsby-plugin-theme-ui`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-sqip`,
+    `gatsby-remark-images-contentful`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `TN Clothes Shop`,
+        short_name: `TN Shop`,
+        description:
+          "Clothes shop",
+        start_url: `/`,
+        background_color: `#0d47a1`,
+        theme_color: `#1a237e`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-contentful`,
       options: {
